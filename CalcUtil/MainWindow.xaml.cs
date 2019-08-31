6d7750fec6fab34d1b1a004c05377fdc80df5aa2
hunter10 <hunter10@naver.com>
2019-08-31 22:49:04 +0900

    First local commit

---
+++ CalcUtil/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalcUtil
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Model currModel;
+        private CompressType currType;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            currType = CompressType.NONE;
+            currModel = Model.NONE;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(currType == CompressType.NONE)
+            {
+                MessageBox.Show("컴프레스 타입을 정해주세요.");
+                return;
+            }
+
+            double d2 = double.Parse(txtBox_D2.Text);
+            double h = double.Parse(txtBox_H.Text);
+
+            Turbine turbine = null;
+            if (currType == CompressType.Retro_Curved)
+            {
+                turbine = new RetroCurved_Turbine();
+            }
+            else if (currType == CompressType.Retro_Curved)
+            {
+                turbine = new RadiallyTipped_Turbine();
+            }
+
+            turbine.Init("", currType, h, d2);
+
+            //lb_D3_val.Content = 1.12 * d2;
+            //lb_D4_val.Content = 1.67 * d2;
+            //lb_F_val.Content = Math.Sqrt(3030 * d2 * h);
+
+            //double a = Math.Pow(d2, 2);
+            //double b = 6.8 * d2 * h;
+            //lb_Di_val.Content = Math.Sqrt(a - b);
+
+            lb_D3_val.Content = turbine.D3();
+            lb_D4_val.Content = turbine.D4();
+            lb_F_val.Content = turbine.F();
+            lb_Di_val.Content = turbine.Di();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idx = combo_compressType.SelectedIndex;
+            switch(idx)
+            {
+                case 0: currType = CompressType.Retro_Curved; break;
+                case 1: currType = CompressType.Radially_Tipped; break;
+            }
+        }
+    }
+}
---
+++ CalcUtil/Turbine.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcUtil
+{
+    public enum Model
+    {
+        NONE,
+        TK_50,
+        KJ66,
+        GR_180,
+        TJE_500
+    }
+
+    public enum CompressType
+    {
+        NONE,
+        Retro_Curved,
+        Radially_Tipped
+    }
+
+    public class Turbine
+    {
+        //private Model currModel;
+        private string currModel;
+        private CompressType currType;
+
+        protected double height;
+        protected double d2;
+
+        protected double d3;
+        protected double d4;
+        protected double f;
+        protected double di;
+
+        protected double angle;
+        protected int bladeNum;
+
+        public Turbine() { }
+
+        public void Init(string _currModel, CompressType _currType, double _height, double _d2)
+        {
+            this.currModel = _currModel;
+            this.currType = _currType;
+
+            this.height = _height;
+            this.d2 = _d2;
+        }
+
+        public virtual double D3() { return d3; }
+        public virtual double D4() { return d4; }
+        public virtual double F() { return f; }
+        public virtual double Di() { return di; }
+    }
+
+    class RetroCurved_Turbine : Turbine
+    {
+        public override double D3()
+        {
+            return (1.12 * d2);
+        }
+
+        public override double D4()
+        {
+            return (1.67 * d2);
+        }
+
+        public override double F()
+        {
+            return Math.Sqrt(3030 * d2 * height);
+        }
+
+        public override double Di()
+        {
+            double a = Math.Pow(d2, 2);
+            double b = 6.8 * d2 * height;
+            return Math.Sqrt(a - b);
+        }
+
+    }
+
+    class RadiallyTipped_Turbine : Turbine
+    {
+        public override double D3()
+        {
+            return (1.1 * d2);
+        }
+
+        public override double D4()
+        {
+            return (1.7 * d2);
+        }
+
+        public override double F()
+        {
+            return Math.Sqrt(2600 * d2 * height);
+        }
+
+        public override double Di()
+        {
+            double a = Math.Pow(d2, 2);
+            double b = 5.3 * d2 * height;
+            return Math.Sqrt(a - b);
+        }
+    }
+}
